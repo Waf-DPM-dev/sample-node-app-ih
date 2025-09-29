353ae71fdd44432fb20d96bbc6cedcf84888ae93
Wafa <wafa.allihaibi@gmail.com>
2025-09-29 16:34:36 +0300

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,11 @@
+// Make console.log a mock so tests using toHaveBeenCalledWith(...) work
 beforeAll(() => {
   jest.spyOn(console, 'log').mockImplementation(() => {});
 });
+
 afterAll(() => {
-  // restore original console.log
   if (console.log && console.log.mockRestore) console.log.mockRestore();
 });
+
+// Optional: no-ops for browser APIs some tests might touch
+Object.defineProperty(window, 'scrollTo', { value: jest.fn(), writable: true });
