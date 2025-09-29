c7db58ae712931c2488c468c2afca85823e5b425
Wafa <wafa.allihaibi@gmail.com>
2025-09-29 16:35:08 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  testEnvironment: 'node',
+  // Use a browser-like environment for client tests
+  testEnvironment: 'jsdom',
+
+  // Make sure the setup file (mocks) loads before every test
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
+  // Coverage output that Sonar will read
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'text-summary'],
+
+  // Match both *.test.js and *.spec.js anywhere
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  setupFilesAfterEnv: ['./jest.setup.js']
 };
+
