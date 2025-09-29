c7223fd8084ccbb3668c14e2699a11d8e2ba02c6
Ubuntu <azureuser@waf-so.5mmjurvkdomunbktgquihhtmxc.oslx.internal.cloudapp.net>
2025-09-29 12:06:10 +0000

    Fix package.json; add Jest CI config and sample test

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,7 @@
 module.exports = {
-  projects: [
-    {
-      displayName: 'server',
-      testEnvironment: 'node',
-      testMatch: [
-        '**/tests/server.test.js',
-        '**/tests/integration.test.js'
-      ],
-      collectCoverageFrom: [
-        'server.js',
-        '!**/node_modules/**',
-        '!**/coverage/**'
-      ],
-      coverageDirectory: 'coverage/server',
-      setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
-    },
-    {
-      displayName: 'client',
-      testEnvironment: 'jsdom',
-      testMatch: [
-        '**/tests/client.test.js'
-      ],
-      collectCoverageFrom: [
-        'public/script.js',
-        '!**/node_modules/**',
-        '!**/coverage/**'
-      ],
-      coverageDirectory: 'coverage/client',
-      setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
-    }
-  ]
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text-summary'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
 };
